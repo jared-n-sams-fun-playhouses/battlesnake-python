737e6994adabcc7a045725c750f5196b063bc7b1
Jared Smith <jared.smith.jrod@gmail.com>
2016-02-20 21:01:11 -0800

    post-mortem

--- app/main.py
+++ app/main.py
@@ -1,32 +1,104 @@
 import bottle
 import os
+import random
+
+directions = ['north','east','south','west']
+
+debug = True
 
 def is_valid(request,move):
+    """
+    Maybe instead of checking what's false, check what's true
+    """
     move_coords = get_direction(move)
-#replace 1 with our snake's head coordinates
-    if (1 + move_coords[0] == request['width']):
+
+    us = get_our_snake(request)
+    our_head = us['coords'][0]
+
+    if debug:
+        print("us: {}".format(us))
+
+    for snake in request['snakes']:
+      if(our_head[1]+1 + move_coords[1] == snake['coords'][1] or our_head[1]-1 + move_coords[1] == snake['coords'][1]):
+        return False
+      if(our_head[0]+1 + move_coords[0] == snake['coords'][0] or our_head[1]-1 + move_coords[1] == snake['coords'][0]):
         return False
 
-    if (1 + move_coords[1] == request['height']):
-   	    return False
+    # north wall
+    if (move == 'north'):
+        if(our_head[1] + move_coords[1] <= 0):
+            return False			  
+
+    # east wall    
+    if (move == 'east'):
+      if(our_head[0] + move_coords[0] >= (request['width'] - 1)):
+        return False
+    
+    # south wall
+    if (move == 'south'):
+        if(our_head[1] + move_coords[1] >= (request['height'] - 1)):
+           return False
+       
+    # west wall
+    if (move == 'west'):
+        if (our_head[0] + move_coords[0] <= 0):
+            return False
 
     return True
 
 def get_direction(move):
-   return {
-       'north' : [0,-1],
-       'south' : [0,1],
-       'west' : [-1,0],
-       'east' : [1,0]
-   }.get(move,'north')
+    return {
+        'north' : [0,-1],
+        'south' : [0,1],
+        'west' : [-1,0],
+        'east' : [1,0]
+    }.get(move,'north')
 
 
 def valid_moves(request):
-    directions = {'north','south','west','east'}
+    good = []
     for move in directions:
-        if not is_valid(request,move):
-           directions.discard(move)
-    return directions
+        if is_valid(request,move):
+            good.append(move)
+    return good
+        
+def get_our_head(request):
+    """
+    We should depricated this and use only get_our_snake
+    """
+    for snake in request['snakes']:
+        if snake['name'] == 'OObuddies':
+            return snake['coords'][0]
+      
+def get_our_snake(request):
+    for snake in request['snakes']:
+        if snake['name'] == 'OObuddies':
+            return snake
+
+def get_food_coord(request):
+    location = get_our_head(request)
+    lowest = 100
+
+    for food in request['food']:
+        headtotal = location[0] - location[1]
+        food_total = food[0] - food[1]
+        diff = head_total - food_total
+        
+        if diff < lowest:
+            lowest = diff
+            closest_food = food
+
+    return closest_food
+
+def is_hungry(request):
+    if request['health'] <= 50:
+        return True
+    return False
+
+def get_taunt():
+    taunts = ['Lean on me', 'Who has food?', 'I got this boiiizzzz', '#SnakeLife', 'You crawl here often?','Dayyyynk Lmao']
+    random.shuffle(taunts)
+    return taunts.pop()
 
 
 @bottle.route('/static/<path:path>')
@@ -36,10 +108,7 @@ def static(path):
 
 @bottle.get('/')
 def index():
-    head_url = '%s://%s/static/head.png' % (
-        bottle.request.urlparts.scheme,
-        bottle.request.urlparts.netloc
-    )
+    head_url = 'http://i.imgur.com/K943feA.png'
 
     return {
         'color': '#00ff00',
@@ -50,11 +119,9 @@ def index():
 @bottle.post('/start')
 def start():
     data = bottle.request.json
-
-    # TODO: Do things with data
-
+    
     return {
-        'taunt': 'battlesnake-python!'
+        'taunt': get_taunt()
     }
 
 
@@ -62,12 +129,13 @@ def start():
 def move():
     data = bottle.request.json
 
-    print(data['width'], data['height'])
-    # TODO: Do things with data
-
+    available = valid_moves(data)
+  
+    random.shuffle(available)
+  
     return {
-        'move': 'north',
-        'taunt': 'battlesnake-python!'
+        'move': available[0],
+        'taunt': get_taunt()
     }
 
 
@@ -75,10 +143,8 @@ def move():
 def end():
     data = bottle.request.json
 
-    # TODO: Do things with data
-
     return {
-        'taunt': 'battlesnake-python!'
+        'taunt': get_taunt()
     }
 
 
